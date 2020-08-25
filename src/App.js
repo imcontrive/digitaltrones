1de5048da3d0303b6e625d4703ef8881917d4492
shubh <psubham94@gmail.com>
2020-08-25 14:26:30 +0530

    add redux, react-router-dom, components, reducers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import "./resets.css";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/constants/constant.js
@@ -0,0 +1,50 @@
+export const _timeslotList = [
+  {
+    slot: "09:00am-10:00am",
+    isBooked: false,
+    hasData: false,
+    _id: "01"
+  },
+  {
+    slot: "10:00am-11:00am",
+    isBooked: false,
+    hasData: false,
+    _id: "02"
+  },
+  {
+    slot: "11:00am-12:00am",
+    isBooked: false,
+    hasData: false,
+    _id: "03"
+  },
+  {
+    slot: "12:00am-01:00pm",
+    isBooked: false,
+    hasData: false,
+    _id: "04"
+  },
+  {
+    slot: "01:00pm-02:00pm",
+    isBooked: false,
+    hasData: false,
+    _id: "05"
+  },
+  {
+    slot: "02:00pm-03:00pm",
+    isBooked: false,
+    hasData: false,
+    _id: "06"
+  },
+  {
+    slot: "03:00pm-04:00pm",
+    isBooked: false,
+    hasData: false,
+    _id: "07"
+  },
+  {
+    slot: "04:00pm-05:00pm",
+    isBooked: false,
+    hasData: false,
+    _id: "08"
+  }
+];
